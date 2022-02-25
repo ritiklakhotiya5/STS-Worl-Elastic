058007eabae9a3f68a51644a4bc60db680877296
vinus.agrawal <vaibhav.yadav@genesistechnologies.in>
2022-02-25 18:56:53 +0530

    as

---
+++ src/test/java/com/example/elasticpoc/ElasticPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.elasticpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
